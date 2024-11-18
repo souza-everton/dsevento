a94ca1d056eaa93d55ff5031a48a1ebbf85490c6
Everton Souza <eve.souzalemos@gmail.com>
2024-11-18 11:06:45 -0300

    Entidade Categoria

---
+++ src/main/java/com/devsuperior/dsevento/entities/Categoria.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsevento.entities;
+
+public class Categoria {
+
+	private Integer id;
+	private String descricao;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
