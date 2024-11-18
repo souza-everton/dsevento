d3be492401fd0e54e45181a8cde9eb2d72724f9a
Everton Souza <eve.souzalemos@gmail.com>
2024-11-18 20:00:31 -0300

    Entity Bloco

--- src/main/java/com/devsuperior/dsevento/entities/Atividade.java
+++ src/main/java/com/devsuperior/dsevento/entities/Atividade.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dsevento.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +26,9 @@ public class Atividade {
 	@ManyToOne
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
+	
+	@OneToMany(mappedBy = "atividade")
+	private List<Bloco> blocos = new ArrayList<>();
 
 	public Atividade() {
 
